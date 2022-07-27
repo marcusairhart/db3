e8f3b0d28e23b18f4091fe9bcb1f38a82f9ecfd2
marcusairhart <74564230+marcusairhart@users.noreply.github.com>
2022-07-27 12:04:13 +0200

    test

---
+++ user_authentication.js
@@ -0,0 +1,30 @@
+connect();
+
+async function connect() {
+    const urlQlikServer = "https://g65jaeuc86rk7ms.eu.qlikcloud.com";
+    const urlLoggedIn = "/api/v1/audits";//Use GET request to see if you are authenticated
+    const urlLogin = "/login";
+    const webIntegrationId = '8k_IjjeD1D0LOF4SkToDmRm5vvWLf5oz';        
+
+    //Check to see if logged in
+    return await fetch(`${urlQlikServer}${urlLoggedIn}`, {
+        credentials: 'include',
+        headers: {                  
+            'Qlik-Web-Integration-ID':webIntegrationId
+        }
+    })
+    .then(async function(response)
+    {
+        //check if user is authenticated; if not, redirect to login page
+		if(response.status===401){
+            const url = new URL(`${urlQlikServer}/login`);
+            url.searchParams.append('returnto', 'https://marcusairhart.github.io/db3/');
+            url.searchParams.append('qlik-web-integration-id', webIntegrationId);
+            window.location.href = url;
+        }	
+    })
+    .catch(function(error)
+    {
+        console.error(error);
+    });	
+}			
